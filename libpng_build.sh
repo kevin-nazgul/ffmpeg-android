42180d82d476d5130dfbaa11a3a65e255ffedca2
kevinhuang <kevin.nazgul@gmail.com>
2019-08-13 15:56:02 +0800

    enable neon by --mfpu=neon, see:       yes|on)          AC_DEFINE([PNG_ARM_NEON_OPT], [2],                    [Enable ARM Neon optimizations])          AC_MSG_WARN([--enable-arm-neon: please specify 'check' or 'api', if]             [you want the optimizations unconditionally pass -mfpu=neon]             [to the compiler.]);;

--- libpng_build.sh
+++ libpng_build.sh
@@ -6,13 +6,6 @@ pushd libpng-1.6.21
 
 make clean
 
-ARM_NEON="no"
-case $1 in
-  armeabi-v7a-neon)
-    ARM_NEON="yes"
-    ;;
-esac
-
 ./configure \
   --with-pic \
   --with-sysroot="$NDK_SYSROOT" \
@@ -20,7 +13,6 @@ esac
   --enable-static \
   --disable-shared \
   --prefix="${TOOLCHAIN_PREFIX}" \
-  --enable-arm-neon="$ARM_NEON" \
   --disable-shared || exit 1
 
 make -j${NUMBER_OF_CORES} install || exit 1
